898c0205412c2dc831c43ce26717a0d4638ac06b
andful <buongiorno19972@gmail.com>
2017-10-21 12:05:57 +0200

    hi

--- project assignment/Views.sql
+++ project assignment/Views.sql
@@ -12,12 +12,6 @@ SELECT E.User_Name, S.Speciality, E.Schedule
 FROM SPECIALIST AS S, EMPLOYEE AS E
 WHERE S.Employee_ID = E.Employee_ID;
 
-CREATE VIEW person AS 
-SELECT users.user_name,
-WHERE users.user_name=emplyee.user_name AND employee.employee_id= manager.employee_id AND employee.employee_id= moderator.employee_id
-GROUP BY users.user_name
-;
-
 
 --DROP VIEW #Name
 --TO see the name of the views : select table_name from INFORMATION_SCHEMA.views WHERE table_schema = ANY (current_schemas(false))
